89af56bd57d9ac934ec700efcc287409b2491ffb
Molik Miah <molik.miah@replgroup.com>
2017-05-15 21:59:32 +0100

    added medium editor, added lodash, setup base methods for local storage Database

---
+++ pad/src/app/core/database.service.ts
@@ -0,0 +1,163 @@
+import { Injectable, OnInit } from '@angular/core';
+import * as _ from 'lodash';
+declare var localStorage: any;
+
+@Injectable()
+export class DatabaseService implements OnInit {
+
+  private DBConfig = {
+    name: 'db_name',
+    prefix: 'v1',
+    secret: 'rn23k=23-1p2[Fwee@£$wFR31Q',
+    lsKey: '!app!pad!' + this.DBConfig.secret
+  };
+
+  public collection;
+  public selectedItem;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.logger('Initializing the app', 1, 2);
+    this.getDB();
+  }
+
+  public getByID(id): any {
+    return _.find(this.collection, item => {
+      return item['id'] === id;
+    });
+  }
+
+  public save(item): void {
+    // check if item already exists in db
+
+          // if it exists, then update valies
+
+          // else, insert in database
+
+    // run saveDB()? synchronise data
+  }
+
+  private saveDB(data): void {
+    localStorage.setItem(this.DBConfig.lsKey, data);
+  }
+
+  private getDB(): void {
+    this.collection = localStorage.getItem(this.DBConfig.lsKey);
+  }
+
+  private logger(message: any, pre?: number, post?: number): void {
+    if (typeof pre === 'number') {
+      for (let i = 0; i < pre; i++) {
+        console.log('');
+      }
+    }
+
+    console.log('> ' + message);
+
+    if (typeof post === 'number') {
+      for (let i = 0; i < post; i++) {
+        console.log('');
+      }
+    }
+  }
+
+  private encrypt(data: any): string {
+    return this.Base64.encode(data);
+  }
+
+  private decrypt(data: string): any {
+    return this.Base64.decode(data);
+  }
+
+  private Base64 = {
+      _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+      encode: function(e) {
+          var t = "";
+          var n, r, i, s, o, u, a;
+          var f = 0;
+          e = this.Base64._utf8_encode(e);
+          while (f < e.length) {
+              n = e.charCodeAt(f++);
+              r = e.charCodeAt(f++);
+              i = e.charCodeAt(f++);
+              s = n >> 2;
+              o = (n & 3) << 4 | r >> 4;
+              u = (r & 15) << 2 | i >> 6;
+              a = i & 63;
+              if (isNaN(r)) {
+                  u = a = 64
+              } else if (isNaN(i)) {
+                  a = 64
+              }
+              t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
+          }
+          return t
+      },
+      decode: function(e) {
+          var t = "";
+          var n, r, i;
+          var s, o, u, a;
+          var f = 0;
+          e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+          while (f < e.length) {
+              s = this._keyStr.indexOf(e.charAt(f++));
+              o = this._keyStr.indexOf(e.charAt(f++));
+              u = this._keyStr.indexOf(e.charAt(f++));
+              a = this._keyStr.indexOf(e.charAt(f++));
+              n = s << 2 | o >> 4;
+              r = (o & 15) << 4 | u >> 2;
+              i = (u & 3) << 6 | a;
+              t = t + String.fromCharCode(n);
+              if (u != 64) {
+                  t = t + String.fromCharCode(r)
+              }
+              if (a != 64) {
+                  t = t + String.fromCharCode(i)
+              }
+          }
+          t = this.Base64._utf8_decode(t);
+          return t
+      },
+      _utf8_encode: function(e) {
+          e = e.replace(/rn/g, "n");
+          var t = "";
+          for (var n = 0; n < e.length; n++) {
+              var r = e.charCodeAt(n);
+              if (r < 128) {
+                  t += String.fromCharCode(r)
+              } else if (r > 127 && r < 2048) {
+                  t += String.fromCharCode(r >> 6 | 192);
+                  t += String.fromCharCode(r & 63 | 128)
+              } else {
+                  t += String.fromCharCode(r >> 12 | 224);
+                  t += String.fromCharCode(r >> 6 & 63 | 128);
+                  t += String.fromCharCode(r & 63 | 128)
+              }
+          }
+          return t
+      },
+      _utf8_decode: function(e) {
+          var t = "";
+          var n = 0;
+          var r = c1 = c2 = 0;
+          while (n < e.length) {
+              r = e.charCodeAt(n);
+              if (r < 128) {
+                  t += String.fromCharCode(r);
+                  n++
+              } else if (r > 191 && r < 224) {
+                  c2 = e.charCodeAt(n + 1);
+                  t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+                  n += 2
+              } else {
+                  c2 = e.charCodeAt(n + 1);
+                  c3 = e.charCodeAt(n + 2);
+                  t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+                  n += 3
+              }
+          }
+          return t;
+      }
+  }
+}
--- pad/src/app/frontpage/frontpage.component.ts
+++ pad/src/app/frontpage/frontpage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var MediumEditor: any;
+
 @Component({
   selector: 'frontpage',
   templateUrl: './frontpage.component.html',
@@ -10,6 +12,52 @@ export class FrontpageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var editor = new MediumEditor('.editable', {
+      toolbar: {
+        buttons: ['bold', 'italic', 'underline', 'strikethrough', 'anchor', 'image', 'quote']
+      }
+    });
+
+    let allNotes = [];
+    allNotes.push(new Note('First Note', 'This is a description...', 'The full contents goes here...'));
+    allNotes.push(new Note('Second Note', 'This is a description...', 'The full contents goes here...'));
+    allNotes.push(new Note('Third Note', 'This is a description...', 'The full contents goes here...'));
+    allNotes.push(new Note('Forth Note', 'This is a description...', 'The full contents goes here...'));
+
+    allNotes.forEach(note => {
+      console.log(note);
+    });
   }
 
+}
+
+class Note {
+
+  readonly ID: string = this.IDGenerater();
+  public attributes: any = {};
+  private meta: any = {
+    createdTS: this.timestamp(),
+    isDeleted: false,
+    private: false
+  };
+
+  constructor(title: string, description: string, content: string) {
+    this.attributes['title'] = title;
+    this.attributes['description'] = description;
+    this.attributes['content'] = content;
+  }
+
+  private IDGenerater(): string {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  }
+
+  private timestamp(): number {
+    return +new Date();
+  }
 }
